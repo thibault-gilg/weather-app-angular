4fcc62764703a4944fac666b1b09d6fe7f830168
thibault-gilg <thibault.gilg@yahoo.fr>
2020-05-22 13:15:00 +0400

    form control added

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from './search.service';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, ValidatorFn, AbstractControl } from '@angular/forms'
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,15 +10,32 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  
-  constructor(private searchService : SearchService) { }
 
+  weatherSearchForm: FormGroup;
+  //Regexp to control the user's input : geographic coordinates (D,D) or word
+  inputRegexp : string = "(^[A-Za-z]+$)|^([-+]?)([0-9]{1,2})(((\.)([0-9]+)(,)))(\s*)(([-+]?)([0-9]{1,3})((\.)([0-9]+))?)$";
+
+  constructor(private searchService: SearchService,
+    private formBuilder: FormBuilder,
+    private router: Router) { }
   ngOnInit(): void {
+    this.initForm();
   }
 
-  onSearchWeather(input: NgForm) : void {
-    const location = input.value['location'];
+  initForm() {
+    this.weatherSearchForm = this.formBuilder.group({
+      location: new FormControl('', [Validators.required, Validators.pattern(this.inputRegexp)])
+    })
+  }
+
+  get location() {
+    return this.weatherSearchForm.get('location');
+  }
+
+  onSubmitSearch(): void {
+    const location = this.weatherSearchForm.value.location;
     this.searchService.searchWeather(location);
+    //this.router.navigate(['/'])
   }
 
 }
